b89382398353e12e00d161a00f62167fef0547d5
Dmytro Ostapovets <dostapovets@unicyb.kiev.ua>
2017-07-11 11:02:34 +0300

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    gulpIf = require('gulp-if'),
+    browserSync = require('browser-sync').create(),
+    useref = require('gulp-useref');
 
 gulp.task('sass', function() {
-    return gulp.src('app/sass/**/*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('./app/css'));
+    return gulp.src('app/scss/**/*.+(scss|sass)')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('sass:watch', function() {
-    gulp.watch('app/sass/**/*.scss', ['sass']);
+gulp.task('useref', function() {
+    return gulp.src('app/*.html')
+        .pipe(gulpIf("*.js"), useref())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    });
+});
+
+gulp.task('default', ['browserSync', 'sass'], function() {
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
 });

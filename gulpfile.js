6de3504e885caad8832f580fd016e0e35b5a48c9
Dmytro Ostapovets <dostapovets@unicyb.kiev.ua>
2017-07-11 11:22:21 +0300

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    del = require('del'),
     gulpIf = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    cssnano = require('gulp-cssnano'),
+    cache = require('gulp-cache'),
+    runSequence = require('run-sequence'),
+    imagemin = require('gulp-imagemin'),
     browserSync = require('browser-sync').create(),
     useref = require('gulp-useref');
 
@@ -14,10 +20,22 @@ gulp.task('sass', function() {
             stream: true
         }))
 });
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+})
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+});
 
 gulp.task('useref', function() {
     return gulp.src('app/*.html')
-        .pipe(gulpIf("*.js"), useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('dist'))
 });
 
@@ -28,9 +46,17 @@ gulp.task('browserSync', function() {
         },
     });
 });
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+})
 
 gulp.task('default', ['browserSync', 'sass'], function() {
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
     gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
 });
+gulp.task('build', function() {
+    runSequence('clean:dist', ['sass', 'useref', 'images', 'fonts'],
+        function() { console.log("Build Succses!"); }
+    )
+})
